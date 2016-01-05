7700fd40ea47a956e6eaa5948b3442f08d140bd6
Bianca Semprit <bsemprit@gmail.com>
2016-01-05 09:47:46 -0500

    Third iteration added and edited second iteration to remove some comments

--- caesar.rb
+++ caesar.rb
@@ -1,35 +1,30 @@
-# def solve_cipher(input) #first iteration
-# 	word = ""
-# 	array1 = input.split("")
-# 	array1.each do |i|
-# 		order = i.ord
-# 		if order == 97
-# 			order = 122
-# 		elsif order == 65
-# 			order = 90	
-# 		else
-# 		order = order - 1
-# 		end	
-# 		word = word + order.chr
-# 	end
-# 	puts word
-# end
-
-# solve_cipher("ifmmp")
-# solve_cipher("ANTHONY")
-# # should return "hello"
-
-def solve_cipher(input, x) #first iteration
+def solve_cipher(input) #first iteration
 	word = ""
-	# make= input.gsub(/[!@#$%^&*()-=_+|;':",.<>?' ]/, '')
-	# puts make
 	array1 = input.split("")
 	array1.each do |i|
 		order = i.ord
-		# if order == 97 && x > 0
-		# 	order = 122 - x
-		# elsif order == 97 && x < 0
-		# 	order = 122 + x
+		if order == 97
+			order = 122
+		elsif order == 65
+			order = 90	
+		else
+		order = order - 1
+		end	
+		word = word + order.chr
+	end
+	puts word
+end
+
+solve_cipher("ifmmp")
+solve_cipher("ANTHONY")
+# should return "hello"
+
+def solve_cipher(input, x) #second iteration
+	word = ""
+
+	array1 = input.split("")
+	array1.each do |i|
+		order = i.ord
 		if order == 32
 			order == 32
 		elsif i.ord + x < 97
@@ -47,3 +42,24 @@ def solve_cipher(input, x) #first iteration
 end
 
 solve_cipher("pb uhdo qdph lv grqdog gxfn", -3)
+
+def solve_cipher(input) #third iteration
+	word = ""
+
+	array1 = input.split("")
+	array1.each do |i|
+		x = -3
+		order = i.ord
+		if order == 32
+			order == 32
+		elsif order - 3 < 97
+			order = 122 + (97 - i.ord)
+		else
+		order = order - 3
+		end	
+		word = word + order.chr
+	end
+	puts word
+end
+
+solve_cipher("pb uhdo qdph lv grqdog gxfn")
