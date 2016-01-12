d765a84cf27e00c4e8709a7bf2096e1effb10dda
Bianca Semprit <bsemprit@gmail.com>
2016-01-12 18:38:47 -0500

    Got blog to display pages and the right posts on the first page. Need next and switcher for pages.

--- Week-1/day_two/blog/blog.rb
+++ Week-1/day_two/blog/blog.rb
@@ -2,7 +2,7 @@ require_relative("lib/blog_class.rb")
 require_relative("lib/post.rb")
 require_relative("lib/Sponsored.rb")
 
-# require 'colorize'
+require 'colorize'
 
 # Post title1
 # **************
@@ -21,7 +21,8 @@ blog = Blog.new
 blog.add_post Post.new("First", Time.now, "First text here")
 blog.add_post Post.new("Third", Time.new(2002, 12, 1), "Third text here")
 blog.add_post Sponsored.new("Second", Time.new(2004, 12, 4), "Second text here")
+# blog.add_post Post.new("Fourth", Time.new(2000, 10, 1), "Last text here")
 
-
+puts "Red".red
 
 blog.publish_front_page
--- Week-1/day_two/blog/lib/blog_class.rb
+++ Week-1/day_two/blog/lib/blog_class.rb
@@ -1,7 +1,10 @@
 class Blog
 	def initialize 
 		@new_posts = []
+		@currentpage = 1
+		@post_per_page = 3
 	end
+
 	def add_post (post)
 		@post = post
 		@new_posts.push(@post)
@@ -10,8 +13,23 @@ class Blog
 	def publish_front_page
 		wholepage = @new_posts.sort {|post_a, post_b| post_b.date <=> post_a.date}
 
-		wholepage.each do |each_post|
+		#which page are you on
+		total_posts = wholepage.length
+		if total_posts > 3
+			@currentpage = ((total_posts - 1) / @post_per_page) + 1
+
+		else
+			@currentpage = 1
+		end
+
+	
+			first_post = (@currentpage - 1)* @post_per_page
+			last_post = first_post + 3
+
+		wholepage[first_post...last_post].each do |each_post|
 			each_post.display
 		end
+
+		puts "\n\n\n#{first_post+1}, #{first_post +2}, #{last_post} "
 	end
 end
