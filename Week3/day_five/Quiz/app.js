40fc721d41d45b60bdf5f473901c77bb2b847791
Bianca Semprit <bsemprit@gmail.com>
2016-01-29 13:55:27 -0500

    Finished Quiz with capitals in terminal exercise.

--- Week3/day_five/Quiz/app.js
+++ Week3/day_five/Quiz/app.js
@@ -1,16 +1,19 @@
-var Questions = require("./lib/question.js")
-var Quiz = require("./lib/quiz.js")
+var Questions = require("./lib/question.js");
+var Quiz = require("./lib/quiz.js");
+var read = require('read');
 
-question1 = new Question(1, "What is the capital of Germany?", "Berlin");
-question2 = new Question(2, "What is the capital of Russia?", "Moscow");
-question3 = new Question(3, "What is the capital of Kenya?", "Nairobi");
-question4 = new Question(4, "What is the capital of Columbia?", "Bogota");
-question5 = new Question(5, "What is the capital of India?", "New Dehli");
-question6 = new Question(6, "What is the capital of Taiwan?", "Taipei");
-question7 = new Question(7, "What is the capital of Cuba?", "Havana");
-question8 = new Question(8, "What is the capital of Puerto Rico?", "San Juan");
-question9 = new Question(9, "What is the capital of Uruaguay?", "Montevideo");
-question10 = new Question(10, "What is the capital of Argentina?", "Buenos Aires");
+question1 = new Questions(1, "What is the capital of Germany?", "Berlin");
+question2 = new Questions(2, "What is the capital of Russia?", "Moscow");
+question3 = new Questions(3, "What is the capital of Kenya?", "Nairobi");
+question4 = new Questions(4, "What is the capital of Columbia?", "Bogota");
+question5 = new Questions(5, "What is the capital of India?", "New Dehli");
+question6 = new Questions(6, "What is the capital of Taiwan?", "Taipei");
+question7 = new Questions(7, "What is the capital of Cuba?", "Havana");
+question8 = new Questions(8, "What is the capital of Puerto Rico?", "San Juan");
+question9 = new Questions(9, "What is the capital of Uruaguay?", "Montevideo");
+question10 = new Questions(10, "What is the capital of Argentina?", "Buenos Aires");
 // question11 = new Question(11, "What is the capital of United States?", "DC")
 
-quiz = new Quiz([question1, question2, question3, question4, question5, question6, question7, question8, question9, question10])
+var quiz = new Quiz([question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]);
+
+quiz.questionManager();
--- Week3/day_five/Quiz/lib/quiz.js
+++ Week3/day_five/Quiz/lib/quiz.js
@@ -1,6 +1,44 @@
+var read = require('read');
 var Quiz = function(questionsArray) {
 	this.questionsArray = questionsArray;
+	this.i = 0;
+	this.currentQuestion = questionsArray[this.i]
+
 }
+
+Quiz.prototype.print= function() {
+	console.log(this.currentQuestion.question);
+}
+
+Quiz.prototype.questionManager= function() {
+	
+	if (this.i < this.questionsArray.length) {
+		var askQuestion = {prompt: `Question ${this.i+1}: ${this.currentQuestion.question}`};
+		read(askQuestion, this.checkAnswer.bind(this));
+	}
+	else {
+		console.log("You now know all the capitals!");
+		console.log("CONGRATS!!!!!!!");
+	};
+};
+
+Quiz.prototype.checkAnswer= function(error, userAnswer) {
+	if (error) {
+		console.log(error.message);
+	}
+	else if(userAnswer === this.currentQuestion.answer) {
+		this.i++;
+		this.currentQuestion = this.questionsArray[this.i]
+		console.log("That's right!");
+		console.log("-----------------------------")
+		this.questionManager();
+	}
+	else {
+		console.log("You're wrong! Try again.");
+		this.questionManager();
+	};
+};
+
 
 
 module.exports = Quiz;
