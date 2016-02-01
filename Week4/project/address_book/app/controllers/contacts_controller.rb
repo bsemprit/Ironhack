626a51f3bf44e3b0009c97104bd60493efe9cde1
Bianca Semprit <bsemprit@gmail.com>
2016-02-01 18:55:48 -0500

    Finished iteration 3 with link.

--- Week4/project/address_book/app/controllers/contacts_controller.rb
+++ Week4/project/address_book/app/controllers/contacts_controller.rb
@@ -9,6 +9,19 @@ class ContactsController < ApplicationController
   end
 
    def create
-    render(:text => "This route is contacts#create.")
+         # Create new Contact from params[:contact]
+	    contact = Contact.new(
+	      :name => params[:contact][:name],
+	      :address => params[:contact][:address],
+	      :phone_number => params[:contact][:phone_number],
+	      :email => params[:contact][:email])
+
+	  	# Now we save the contact
+	    contact.save
+
+	    # Render contact's attributes
+	    # render(:text => contact.attributes)
+
+	    redirect_to("/contacts")
   end
 end
--- Week4/project/address_book/config/routes.rb
+++ Week4/project/address_book/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
    post("/contacts", :to => "contacts#create")
 
+   get "/contacts" => "contacts#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
