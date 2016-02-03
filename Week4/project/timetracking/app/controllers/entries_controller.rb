80e5c1e564f3a0cdf7428a10667340154ea69fed
Bianca Semprit <bsemprit@gmail.com>
2016-02-03 12:18:21 -0500

    Added strong parameters and errors for new entries page

--- Week4/project/timetracking/app/controllers/entries_controller.rb
+++ Week4/project/timetracking/app/controllers/entries_controller.rb
@@ -14,4 +14,21 @@ class EntriesController < ApplicationController
   	@project = Project.find params[:project_id]
   	@entry = @project.entries.new
   end
+
+  def create
+	@project = Project.find params[:project_id]
+  	@entry = @project.entries.new entry_params
+
+  	if entry.save
+  		redirect_to action: "index", controller: "entries", project_id: @project_id
+  	else
+  		render "new"
+  	end
+  end
+
+private
+
+  def entry_params
+  	params.require(:entry).permit(:hours, :date, :minutes)
+  end
 end
